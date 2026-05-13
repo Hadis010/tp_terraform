82fb01736ea0b9a97dbf55c5a476a347fde6eb08
Hadis010 <hafsadini001@gmail.com>
2026-05-13 10:59:57 +0200

    feat(github): add docs repo + topics on app repo

--- 02-github/main.tf
+++ 02-github/main.tf
@@ -4,6 +4,7 @@ resource "github_repository" "app" {
   visibility  = "public"
   has_issues  = true
   auto_init   = true
+  topics = ["terraform", "devops", "s8"]
 }
 
 resource "github_branch_protection" "main" {
@@ -20,4 +21,14 @@ resource "github_actions_secret" "db_url" {
   repository      = github_repository.app.name
   secret_name     = "DATABASE_URL"
   plaintext_value = var.db_url
+}
+
+resource "github_repository" "docs" {
+  name        = "${var.project_name}-docs"
+  description = "Documentation - géré par Terraform"
+  visibility  = "public"
+  has_issues  = false
+  has_wiki    = true
+  auto_init   = true
+  topics      = ["terraform", "devops"]
 }
