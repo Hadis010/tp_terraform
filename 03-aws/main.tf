08f2ccc6a62e707ac38cab52703d16d96bf65c38
Hadis010 <hafsadini001@gmail.com>
2026-05-13 12:04:42 +0200

    feat(aws): add S3

--- 03-aws/main.tf
+++ 03-aws/main.tf
@@ -90,3 +90,24 @@ resource "aws_instance" "web" {
 
   tags = { Name = "${var.project_name}-web" }
 }
+
+resource "aws_s3_bucket" "assets" {
+  bucket = "${var.project_name}-assets"
+  tags   = { Name = "${var.project_name}-assets" }
+}
+
+resource "aws_s3_bucket_versioning" "assets" {
+  bucket = aws_s3_bucket.assets.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "assets" {
+  bucket = aws_s3_bucket.assets.id
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
